c4faf243ec50e37f955633bab39e01e8b0e051d9
luiza <d.luiza@aluno.ifsp.edu.br>
2025-03-25 16:36:07 -0300

    Commit listar ctt

--- agenda.py
+++ agenda.py
@@ -1,1 +1,18 @@
+def adicionar_contato(contatos):
+    nome = input("Digite o nome do contato: ")
+    telefone = input("Digite o telefone do contato: ")
+    favorito = input("O contato é favorito? (s/n): ").strip().lower() == 's'
+    
+    contatos[nome] = {'telefone': telefone, 'favorito': favorito}
+    print(f"Contato {nome} adicionado com sucesso!")
+
+def listar_contatos(contatos):
+    if not contatos:
+        print("Não há contatos cadastrados.")
+    else:
+        for nome in sorted(contatos):
+            telefone = contatos[nome]['telefone']
+            favorito = "Favorito" if contatos[nome]['favorito'] else "Não Favorito"
+            print(f"{nome} - {telefone} - {favorito}")
+
 
